5a11b6263e5449c00405a2958512d28ccc8f3aca
yejinleee <yejin9487@gmail.com>
2023-04-03 20:49:15 +0900

    feat: createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Concept from './pages/Concept';
+import Site from './pages/Site';
+import Analysis from './pages/Analysis';
+import NotFound from './pages/NotFound';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound/>,
+    children: [
+      {path: '/concept', element: <Concept/>},
+      {path: '/site', element: <Site/>},
+      {path: '/analysis', element: <Analysis/>},
+    ],
+  }
+])
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/Concept.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Concept = () => {
+    return (
+        <div>
+            Concept
+        </div>
+    )
+}
+export default Concept;
