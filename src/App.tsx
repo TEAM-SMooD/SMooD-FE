fadb2b782a64b4ccf618520cb147fdb9f9b8f0c2
yejinleee <yejin9487@gmail.com>
2023-04-04 00:06:02 +0900

    feat: route from Home

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,23 @@ const router = createBrowserRouter([
     path: '/',
     element: <Home />,
     errorElement: <NotFound/>,
-    children: [
-      {path: '/concept', element: <Concept/>},
-      {path: '/site', element: <Site/>},
-      {path: '/analysis', element: <Analysis/>},
-    ],
-  }
+    // children: [
+    //   {path: '/concept', element: <Concept/>},
+    //   // {path: '/site', element: <Site/>},
+    //   {path: '/analysis', element: <Analysis/>},
+    // ], 
+    // 여기 <Home /> 내에 <Outlet>으로 위치 준 부분에 바뀌며 들어올 자식 컴포넌트들
+  },
+  {
+    path: '/site',
+    element: <Site />,
+    errorElement: <NotFound/>,
+  },
+  {
+    path: '/concept',
+    element: <Concept />,
+    errorElement: <NotFound/>,
+  },
 ])
 function App() {
   return (
