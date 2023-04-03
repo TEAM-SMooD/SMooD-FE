002d150fd68ab857cd3a97e0b382069e43f10c07
yejinleee <yejin9487@gmail.com>
2023-04-04 00:33:23 +0900

    feat: prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,68 @@
 import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import home_cream from '../assets/home_cream.jpg';
-import home_logo from '../assets/home_logo.png';
+import home_cream from "../assets/home_cream.jpg";
+import home_logo from "../assets/home_logo.png";
 import { designSystem } from "../assets/designSystem";
 const Home = () => {
     const navigate = useNavigate();
+
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-            <img src={home_cream} style={{width:'100%',  zIndex: '-1', maxHeight: '100vh'}} alt="" />
-            <img src={home_logo} style={{width: '13%',left: 'calc(50vw - 6.5%)', top: 'calc(50vh - 6.5%)' }}/>
-            <div style={{display: 'flex', gap:'100px', marginTop: '3rem' }}>
-               <div style={{width: '19rem', height: '17rem', display: 'flex', justifyContent: 'center', alignItems: 'center', background: designSystem.colors.lightgrey}} 
-               onClick={() => navigate('/concept')} >컨셉 추천</div>
-               <div style={{width: '19rem', height: '17rem', display: 'flex', justifyContent: 'center', alignItems: 'center', background: designSystem.colors.lightgrey}}
-               onClick={() => navigate('/site')} >지역 추천</div>
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+            }}
+        >
+            <img
+                src={home_cream}
+                style={{ width: "100%", zIndex: "-1", maxHeight: "100vh" }}
+                alt=""
+            />
+            <img
+                src={home_logo}
+                style={{
+                    width: "13%",
+                    left: "calc(50vw - 6.5%)",
+                    top: "calc(50vh - 6.5%)",
+                }}
+            />
+            <div style={{ display: "flex", gap: "100px", marginTop: "3rem" }}>
+                <div
+                    style={{
+                        width: "19rem",
+                        height: "17rem",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        background: designSystem.colors.lightgrey,
+                    }}
+                    onClick={() => navigate("/concept")}
+                >
+                    컨셉 추천
+                </div>
+                <div
+                    style={{
+                        width: "19rem",
+                        height: "17rem",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        background: designSystem.colors.lightgrey,
+                    }}
+                    onClick={() => navigate("/site")}
+                >
+                    지역 추천
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 export default Home;
 
-{/*
+{
+    /*
             <div>
                 <img src={home_cream} style={{width:'100%', position: 'fixed', zIndex: '-1', maxHeight: '100vh'}} alt="" />
             </div>
@@ -31,4 +73,5 @@ export default Home;
                <button onClick={() => navigate('/site')} >지역 추천</button>
             </div>
         </div>
-*/}
+*/
+}
