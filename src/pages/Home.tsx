d188c1c8965b55cc4f1b93ab72041b16c640f688
yejinleee <yejin9487@gmail.com>
2023-04-04 13:35:33 +0900

    feat: install styled-components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,76 +2,23 @@ import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import home_cream from "../assets/home_cream.jpg";
 import home_logo from "../assets/home_logo.png";
-import { designSystem } from "../assets/designSystem";
+import { StHome, StCreamImg, StLogo, SThomeBtn } from "../styles/HomeST";
 const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-            }}
-        >
-            <img
-                src={home_cream}
-                style={{ width: "100%", zIndex: "-1", maxHeight: "100vh" }}
-                alt=""
-            />
-            <img
-                src={home_logo}
-                style={{
-                    width: "13%",
-                    left: "calc(50vw - 6.5%)",
-                    top: "calc(50vh - 6.5%)",
-                }}
-            />
+        <StHome>
+            <StCreamImg src={home_cream} />
+            <StLogo src={home_logo} />
             <div style={{ display: "flex", gap: "100px", marginTop: "3rem" }}>
-                <div
-                    style={{
-                        width: "19rem",
-                        height: "17rem",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        background: designSystem.colors.lightgrey,
-                    }}
-                    onClick={() => navigate("/concept")}
-                >
+                <SThomeBtn onClick={() => navigate("/concept")}>
                     컨셉 추천
-                </div>
-                <div
-                    style={{
-                        width: "19rem",
-                        height: "17rem",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        background: designSystem.colors.lightgrey,
-                    }}
-                    onClick={() => navigate("/site")}
-                >
+                </SThomeBtn>
+                <SThomeBtn onClick={() => navigate("/site")}>
                     지역 추천
-                </div>
+                </SThomeBtn>
             </div>
-        </div>
+        </StHome>
     );
 };
 export default Home;
-
-{
-    /*
-            <div>
-                <img src={home_cream} style={{width:'100%', position: 'fixed', zIndex: '-1', maxHeight: '100vh'}} alt="" />
-            </div>
-            <img src={home_logo} style={{width: '13%', position: 'fixed',left: 'calc(50vw - 6.5%)', top: 'calc(50vh - 6.5%)' }}/>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'fixed', bottom: '0', width: '100%'}}> 
-            <div>
-               <button onClick={() => navigate('/concept')} >컨셉 추천</button>
-               <button onClick={() => navigate('/site')} >지역 추천</button>
-            </div>
-        </div>
-*/
-}
---
+++ src/styles/designSystem.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    red: "#FF504B",
+    lightgrey: "#B0B0B0",
+    brightgrey: "#F5F5F5",
+};
