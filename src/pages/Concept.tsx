79831594e9ae728e0083218cdd32c7e50864f675
yejinleee <yejin9487@gmail.com>
2023-04-05 22:14:44 +0900

    style: remove comments

--- src/pages/Concept.tsx
+++ src/pages/Concept.tsx
@@ -5,40 +5,7 @@ const Concept = () => {
     return (
         <>
             <Layout idx={1}>
-                <div>그리구 내용이면?1</div>
-                <div>그리구 내용이면?2</div>
-                <div>그리구 내용이면?3</div>
-                <div>그리구 내용이면?4</div>
-                <div>그리구 내용이면?5</div>
-                <div>그리구 내용이면?6</div>
-                <div>그리구 내용이면7?</div>
-                <div>그리구 내용이면?8</div>
-                <div>그리구 내용이면?9</div>
-                <div>그리구 내용이면?tlq</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
-                <div>그리구 내용이면?</div>
+                <div>컨셉 추천 본문</div>
             </Layout>
         </>
     );
