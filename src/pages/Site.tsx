1c0177ad69a2c0fa13e4f2ca15f22db892bf9351
yejinleee <yejin9487@gmail.com>
2023-04-14 15:17:19 +0900

    feat: Site 요소들 배치

--- src/pages/Site.tsx
+++ src/pages/Site.tsx
@@ -1,14 +1,99 @@
 import React, { useState } from "react";
 import Layout from "./Layout";
 import ReportLayout from "./ReportLayout";
+import { StSelectbox } from "../styles/ConceptST";
+import ic_concept from "../assets/ic_concept.png";
+import ic_arrow from "../assets/ic_arrow.png";
+import { colors } from "../styles/designSystem";
 
 const Site = () => {
-    const [a, setA] = useState("z");
     const Test = () => {
         return (
             <>
-                <div onClick={() => setA("asdf")}>여기 업종이랑 뭐 선택</div>
-                <div>분석하기버튼</div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-around",
+                    }}
+                >
+                    <StSelectbox
+                        style={{
+                            width: "30%",
+                            margin: "0",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <div
+                            style={{
+                                padding: "3% 5%",
+                                display: "grid",
+                                width: "100%",
+                                gridTemplateColumns: "25px auto 25px",
+                                alignItems: "center",
+                            }}
+                        >
+                            <img
+                                src={ic_concept}
+                                style={{ width: "19px", height: "19px" }}
+                            />
+                            <div>업종을 선택하세요</div>
+                            <img
+                                src={ic_arrow}
+                                style={{
+                                    rotate: "180deg",
+                                    width: "24px",
+                                }}
+                            />
+                        </div>
+                    </StSelectbox>
+                    <StSelectbox
+                        style={{
+                            width: "30%",
+                            margin: "0",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <div
+                            style={{
+                                padding: "3% 5%",
+                                display: "grid",
+                                width: "100%",
+                                gridTemplateColumns: "25px auto 25px",
+                                alignItems: "center",
+                            }}
+                        >
+                            <img
+                                src={ic_concept}
+                                style={{ width: "19px", height: "19px" }}
+                            />
+                            <div>컨셉을 선택하세요</div>
+                            <img
+                                src={ic_arrow}
+                                style={{
+                                    rotate: "180deg",
+                                    width: "24px",
+                                }}
+                            />
+                        </div>
+                    </StSelectbox>
+                </div>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <div
+                        style={{
+                            width: "110px",
+                            background: "#D9D9D9",
+                            color: "#727272",
+                            display: "flex",
+                            justifyContent: "center",
+                            height: "37px",
+                            alignItems: "center",
+                            borderRadius: "10px",
+                        }}
+                    >
+                        분석하기
+                    </div>
+                </div>
             </>
         );
     };
@@ -16,7 +101,7 @@ const Site = () => {
         <>
             <Layout idx={2}>
                 <ReportLayout title="지역 추천" childrenSelectWrap={<Test />}>
-                    <div>{a}여기는 리포트</div>
+                    <div>여기는 리포트</div>
                 </ReportLayout>
             </Layout>
         </>
