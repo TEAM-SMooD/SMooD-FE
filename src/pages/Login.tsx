9c9f911d1b4a05ac3744d9dc1f05db140635f9e2
yejinleee <yejin9487@gmail.com>
2023-04-17 20:02:22 +0900

    TEST: axios post

---
+++ src/pages/Login.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { Container, Grid, Text } from "../Elements";
+// import { actionCreators as userActions} from "../Redux/Modules/User";
+import axios from "axios";
+const Login = () => {
+    // const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<any>(null);
+    const href = window.location.href;
+    let params = new URL(document.URL).searchParams;
+    let code = params.get("code");
+    useEffect(() => {
+        const postCode = async () => {
+            try {
+                setError(null);
+                setLoading(true);
+                const res = await axios.post(
+                    "/api/oauth/token",
+                    JSON.stringify({ code })
+                );
+                console.log("asdf?");
+            } catch (err) {
+                setError(err);
+                console.log(err);
+            }
+            setLoading(false);
+        };
+        postCode();
+    }, []);
+
+    // React.useEffect(async () => {
+    //     await dispatch(userActions.kakaoLogin(code));
+    // }, []);
+
+    return (
+        <>{loading ? <p>{code}</p> : <p>대기중</p>}</>
+        // <Container>
+        //     <Grid>
+        //         <Text>잠시만 기다려 주세요! 로그인 중입니다.</Text>
+        //     </Grid>
+        // </Container>
+    );
+};
+
+export default Login;
