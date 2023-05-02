e631d334de6b20ceaca552c53b309348e7667843
yejinleee <yejin9487@gmail.com>
2023-05-02 15:33:53 +0900

    BUG : 서버 주소 이슈

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,20 @@
+// const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+// const app = express();
+
+// app.use(
+//     "/api",
+//     createProxyMiddleware({
+//         target: "http://3.39.182.90:8080",
+//         changeOrigin: true,
+//     })
+// );
+// app.listen(3000);
 
 module.exports = function (app) {
     app.use(
-        "/api",
-        createProxyMiddleware({
-            target: "http://localhost:8080",
+        createProxyMiddleware("/api", {
+            target: "http://3.39.182.90:8080",
             changeOrigin: true,
         })
     );
